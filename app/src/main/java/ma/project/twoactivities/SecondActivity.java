cc5b9161cc3c1eb441867f87938cdc3f8c63dc36
youssefBenkamoun <youssefbenkamoun01@gmail.com>
2022-10-10 15:19:54 +0100

    first commit

--- app/src/main/java/ma/project/twoactivities/SecondActivity.java
+++ app/src/main/java/ma/project/twoactivities/SecondActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     private TextView textView;
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +19,6 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra("message");
         textView = findViewById(R.id.text_message);
         textView.setText(message);
+        Log.d(LOG_TAG,"HI");
     }
 }
