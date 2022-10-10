d74aa6e8ebb7776dc7305528a69283eb6b1dd90a
youssefBenkamoun <youssefbenkamoun01@gmail.com>
2022-10-10 21:20:12 +0100

    reply version

--- app/src/main/java/ma/project/twoactivities/MainActivity.java
+++ app/src/main/java/ma/project/twoactivities/MainActivity.java
@@ -8,26 +8,47 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    //private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private Button send;
     private EditText txtMessage;
+    private TextView reply_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        reply_text = findViewById(R.id.text_reply);
         send = findViewById(R.id.button_main);
         txtMessage = findViewById(R.id.editText_main);
+        TextView shown = findViewById(R.id.textView_reply);
+        shown.setText("Reply Received");
+        Intent intent1 = getIntent();
+
+
+        if(intent1 != null){
+
+            String reply1=intent1.getStringExtra("reply");
+            if(reply1 == null){
+                shown.setText("");
+            }
+            reply_text.setText(reply1);
+
+        }
+
+
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(LOG_TAG,"Button clicked !!");
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 String msg = txtMessage.getText().toString();
                 intent.putExtra("message", msg);
                 startActivity(intent);
             }
         });
+
+
     }
 }
--- app/src/main/java/ma/project/twoactivities/SecondActivity.java
+++ app/src/main/java/ma/project/twoactivities/SecondActivity.java
@@ -5,20 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     private TextView textView;
+    private EditText editText;
+    private Button replyButton;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        textView = findViewById(R.id.text_message);
         Intent intent = getIntent();
         String message = intent.getStringExtra("message");
-        textView = findViewById(R.id.text_message);
         textView.setText(message);
+        replyButton = findViewById(R.id.button_reply);
+        editText = findViewById(R.id.editText_reply);
         Log.d(LOG_TAG,"HI");
+        replyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(SecondActivity.this , MainActivity.class);
+                String reply = editText.getText().toString();
+                intent1.putExtra("reply",reply);
+                startActivity(intent1);
+            }
+        });
+
     }
 }
